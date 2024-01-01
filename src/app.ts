9ac07fbe43435948395dd8de6eb3280d3eabe9ee
Naveen Dubey <naveen.parasi@gmail.com>
2024-01-01 23:13:54 +0530

    test lint staged 2

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-
 const addition = (a: number, b: number): number => {
   return a + b;
 };
-
+const myObj = {
+  name: 'Naveen',
+  age: 30,
+  addresses: 'snnst',
+};
+myObj.addresses = 'Gandhinagar';
 const number1: number = 5;
 const number2: number = 10;
 addition(number1, number2);
 // const result: number = addition(number1, number2);
-
-// console.log('The result is %d', result);
