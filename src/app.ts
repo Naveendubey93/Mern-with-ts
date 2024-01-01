bd581eb4038e0f90b376ca5e2ab58ae8df9bf56d
Naveen Dubey <naveen.parasi@gmail.com>
2024-01-01 21:00:50 +0530

    changes for git hook setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+
+const addition = (a: number, b: number): number => {
+  return a + b;
+};
+
+const number1: number = 5;
+const number2: number = 10;
+addition(number1, number2);
+// const result: number = addition(number1, number2);
+
+// console.log('The result is %d', result);
