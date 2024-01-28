311325b9a066d82371ba27e6c47dd91f35bc5cab
Naveen Dubey <naveen.parasi@gmail.com>
2024-01-28 22:02:04 +0530

    added test for unique email

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from './app';
 import { Config } from './config';
+import { AppDataSource } from './config/data-source';
 import logger from './config/logger';
 const startServer = async () => {
   try {
+    await AppDataSource.initialize();
+    logger.info('Database connected successfully');
     app.listen(Config.PORT, () => {
       logger.info(`Listening on ${Config.PORT}`);
     });
