8f16400ab2cd099f48721465ce779443b94bcc25
Naveen Dubey <naveen.parasi@gmail.com>
2024-11-02 09:56:22 +0530

    added cors support

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,14 @@ import { HttpError } from 'http-errors';
 import authRouter from './routes/auth';
 import tenantRouter from './routes/tenant';
 import userRouter from './routes/user';
+import cors from 'cors';
 const app = express();
+app.use(
+  cors({
+    origin: ['http://localhost:5174'],
+    credentials: true,
+  }),
+);
 app.use(express.static('public'));
 app.use(cookieParser());
 app.get('/', async (req, res) => {
